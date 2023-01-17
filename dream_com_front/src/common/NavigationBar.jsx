88fb9881bc5af621e365d2f24e0fc5ecf6e1c2a9
김형도88 <gudeh880@naver.com>
2023-01-17 18:20:20 +0900

    카테고리 수정및 호버적용 완성

--- dream_com_front/src/common/NavigationBar.jsx
+++ dream_com_front/src/common/NavigationBar.jsx
@@ -10,19 +10,62 @@ function NavigationBar() {
 
     const [textLine, setTextLine] = useState(1)
     const [userLine, setUserLine] = useState(1)
+    const [bestLine, setBestLine] = useState(1)
+    const [specialLine, setSpecialLine] = useState(1)
+    const [oneLine, setOneLine] = useState(1)
+    const [zoneLine, setZoneLine] = useState(1)
+    const [gLine, setGLine] = useState(1)
+
 
 
     return (
         <Navbar expand="lg">
-            <div className={"container"} >
-                <Navbar.Brand href="#">쇼핑 딜</Navbar.Brand>
+            <div className={"container"} style={{marginLeft:"175px"}} >
+
                 <Navbar.Toggle aria-controls="navbarScroll" />
                 <Navbar.Collapse id="navbarScroll">
                     <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll>
-                        <Nav.Link href="#action1">컴퓨터</Nav.Link>
-                        <Nav.Link href="#action2">데스크탑</Nav.Link>
-                        <Nav.Link href="#action2">노트북</Nav.Link>
-                        <Nav.Link href="#action2">Link</Nav.Link>
+                        <a><li style={{listStyle: "none"}}
+                               onMouseOver={() => setBestLine(0)}
+                               onMouseOut={() => setBestLine(1)}
+                        >
+                            <Nav.Link href="#action1" style={bestLine ? {textDecorationLine: "none"} : {textDecorationLine: "underline"}}>베스트</Nav.Link>
+
+                        </li>
+                        </a>
+                        <a><li style={{listStyle: "none"}}
+                               onMouseOver={() => setSpecialLine(0)}
+                               onMouseOut={() => setSpecialLine(1)}
+                        >
+                            <Nav.Link href="#action1" style={specialLine ? {textDecorationLine: "none"} : {textDecorationLine: "underline"}}>특가상품</Nav.Link>
+
+                        </li>
+                        </a>
+                        <a><li style={{listStyle: "none"}}
+                               onMouseOver={() => setOneLine(0)}
+                               onMouseOut={() => setOneLine(1)}
+                        >
+                            <Nav.Link href="#action1" style={oneLine ? {textDecorationLine: "none"} : {textDecorationLine: "underline"}}>11번가</Nav.Link>
+
+                        </li>
+                        </a>
+                        <a><li style={{listStyle: "none"}}
+                               onMouseOver={() => setZoneLine(0)}
+                               onMouseOut={() => setZoneLine(1)}
+                        >
+                            <Nav.Link href="#action1" style={zoneLine ? {textDecorationLine: "none"} : {textDecorationLine: "underline"}}>컴퓨존</Nav.Link>
+
+                        </li>
+                        </a>
+                        <a><li style={{listStyle: "none"}}
+                               onMouseOver={() => setGLine(0)}
+                               onMouseOut={() => setGLine(1)}
+                        >
+                            <Nav.Link href="#action1" style={gLine ? {textDecorationLine: "none"} : {textDecorationLine: "underline"}}>지마켓</Nav.Link>
+
+                        </li>
+                        </a>
+
 
 
                     </Nav>
@@ -38,7 +81,7 @@ function NavigationBar() {
 
                             </li>
                             </a>
-                            <a><li style={{listStyle: "none"}}
+                            <a><li style={{listStyle: "none" ,marginRight:"-250px"}}
                                    onMouseOver={() => setUserLine(0)}
                                    onMouseOut={() => setUserLine(1)}
                             >
