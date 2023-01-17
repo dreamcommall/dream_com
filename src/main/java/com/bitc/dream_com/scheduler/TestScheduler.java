458ff5d436109a5e3818209e68a45a3a821159ec
yang <ymh0052@naver.com>
2023-01-17 18:21:19 +0900

    클릭 수 스케줄러

--- src/main/java/com/bitc/dream_com/scheduler/TestScheduler.java
+++ src/main/java/com/bitc/dream_com/scheduler/TestScheduler.java
@@ -1,5 +1,34 @@
 package com.bitc.dream_com.scheduler;
 
-public class TestScheduler {
+import com.bitc.dream_com.dto.ProductDto;
+import com.bitc.dream_com.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 
+import java.util.List;
+
+@Component
+@Controller
+public class TestScheduler {
+    @Autowired
+    private TestService testService;
+
+//    최근 클릭 수 3시간마다 업데이트 스케줄러
+//    최종 수정일 2023-01-17
+//    최종 작성자 : 양민호
+    @Scheduled(cron = "0 0/1 * * * *")
+    public void test() throws Exception {
+//        클릭 수 높은 10개 가져오기
+        List<ProductDto> products = testService.getTopClickedProduct();
+
+//        hitcount에 들어가 있던 데이터 삭제
+        testService.deleteHitCount();
+
+        testService.updateClickCount(products);
+//        map 내용으로 hitcount 업데이트
+//        testService.updateClickCount(map);
+        System.out.println("업데이트 완료");
+    }
 }
