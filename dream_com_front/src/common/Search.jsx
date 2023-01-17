96b296a1646e31b2686c12689880d9f6eef2a75b
김형도88 <gudeh880@naver.com>
2023-01-17 18:21:33 +0900

    검색창 크기 수정 완료/ 이미지 정착완료

--- dream_com_front/src/common/Search.jsx
+++ dream_com_front/src/common/Search.jsx
@@ -6,13 +6,13 @@ function Search() {
     return (
         <Form className="d-flex" >
             <Form.Control
-                style={{borderRadius:"25px"}}
+                style={{borderRadius:"25px",marginTop:"15px"}}
                 type="search"
                 placeholder="Search"
                 className="me-2"
                 aria-label="Search"
             />
-            <Button type={"button"} style={{backgroundColor:"white", border:"none"}}><img style={{width:"40%", paddingRight:"10px"}}  src={"/images/search.png"}/></Button>
+            <Button type={"button"} style={{backgroundColor:"white", border:"none"}}><img style={{width:"63%", paddingRight:"10px", marginLeft:"-20px",marginTop:"10px"}}  src={"/images/search.png"}/></Button>
 
         </Form>
     );
