1bebbbf568cbfad7336d665a067a504a8d8c9814
김형도88 <gudeh880@naver.com>
2023-01-18 16:46:28 +0900

    실시간 키워드 미수정

--- dream_com_front/src/common/Keyword.jsx
+++ dream_com_front/src/common/Keyword.jsx
@@ -1,97 +1,49 @@
 import React, {useEffect, useRef, useState} from "react";
+
 import upIcon from "../up.png"
 import downIcon from "../down.png"
-
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 
 
 function Keyword() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    const [rankList, setRankList] = useState([]);
 
-    const [isOpen, setIsOpen] = useState(false);
-    const openModalHandler = () => {
-        setIsOpen(!isOpen);
-    };
-
-    useEffect(()=> {
-        setRankList([
-            {name:'컴퓨터'},
-            {name:'마우스'},
-            {name:'모니터'},
-            {name:'cpu'},
-            {name:'그래픽카드'},
-            {name:'램'},
-            {name:'USB'},
-            {name:'마우스패드'},
-            {name:'장패드'},
-            {name:'충전기'},
-        ])
-    },[])
-    
-
-
-
-
-    const ref = useRef();
-    useEffect(() => {
-        const checkIfClickedOutside = (e) => {
-            if (isMenuOpen && ref.current && !ref.current.contains(e.target)) {
-                setIsMenuOpen(false);
-
-            }
-        };
-        document.addEventListener("click", checkIfClickedOutside);
-        return () => {
-            document.removeEventListener("click", checkIfClickedOutside);
-        };
-    }, [isMenuOpen]);
     return (
-        <div className="wrapper" ref={ref}>
-            <button  onClick={() => {
-                setIsMenuOpen(!isMenuOpen);
-
-            }} style={{marginTop: "14px", backgroundColor:"white", border:"none"}}>
+        <div>
 
 
-                <img style={{width: "20px", marginTop:"8px"}} src={isMenuOpen == false ? downIcon : upIcon}/>
+            {/*<button id={"dropdown-item-button"} onClick={() => {*/}
+            {/*    setIsMenuOpen(!isMenuOpen);*/}
+
+            {/*}} style={{marginTop: "14px", backgroundColor:"white", border:"none"}}>*/}
 
 
-            </button>
+            {/*    <img style={{width: "20px", marginTop:"8px"}} src={isMenuOpen == false ? downIcon : upIcon}/>*/}
+            {/*</button>*/}
 
-            {isMenuOpen && (
-                <ul className="list" style={{listStyle: "none", display: "block", height: "1px"}}>
-                    {/*<div style={{marginTop:"10px", cursor:"pointer"}}>&times;</div>*/}
-                    {rankList.map((ranking,index)=> {
-                        return <li key={index} className={'list-item'} style={{listStyle:"left"}}>{ranking.name}</li>
-                    })}
-                </ul>
-            )}
+<div className={"row"} style={{marginLeft:"-52px", marginTop:"20px"}}>
+    <div className={"col-8"}>
+    <input type={"text"} />
+    </div>
+    <div className={"col-1"} >
+            <DropdownButton id="dropdown-basic-button" variant="white" style={{border:"none"}}>
 
+                <Dropdown.Header>실시간 키워드</Dropdown.Header>
+                <Dropdown.Item eventKey="1">컴퓨터</Dropdown.Item>
+                <Dropdown.Item eventKey="2">LG 그램</Dropdown.Item>
+                <Dropdown.Item eventKey="3">삼성 컴퓨터</Dropdown.Item>
+                <Dropdown.Item eventKey="3">LG 컴퓨터</Dropdown.Item>
+                <Dropdown.Item eventKey="3">노트북</Dropdown.Item>
+                <Dropdown.Item eventKey="3">마우스</Dropdown.Item>
+                <Dropdown.Item eventKey="3">모니터</Dropdown.Item>
+                <Dropdown.Item eventKey="3">최신 모니터</Dropdown.Item>
 
+            </DropdownButton>
+    </div>
+</div>
 
-            {/*{isMenuOpen && (*/}
-            {/*    <ul className="list" style={{listStyle: "none", display: "block", height: "1px"}}>*/}
-            {/*        {rankList.map(function (ranking, index) {*/}
-            {/*            return <li key={index} className={'list-item'}>{index+1} : {ranking.name}</li>*/}
-            {/*        })}*/}
-            {/*    </ul>*/}
-            {/*)}*/}
-
-            {/*{isMenuOpen && (*/}
-            {/*    <ul className="list" style={{listStyle: "none", display: "block", height: "1px"}}>*/}
-            {/*        <li className="list-item">1 : Dropdown option 1</li>*/}
-            {/*        <li className="list-item">Dropdown option 2</li>*/}
-            {/*        <li className="list-item">Dropdown option 3</li>*/}
-            {/*        <li className="list-item">Dropdown option 4</li>*/}
-            {/*        <li className="list-item">Dropdown option 4</li>*/}
-            {/*        <li className="list-item">Dropdown option 4</li>*/}
-            {/*        <li className="list-item">Dropdown option 4</li>*/}
-            {/*        <li className="list-item">Dropdown option 4</li>*/}
-            {/*        <li className="list-item">Dropdown option 4</li>*/}
-            {/*        <li className="list-item">Dropdown option 4</li>*/}
-            {/*    </ul>*/}
-            {/*)}*/}
         </div>
     );
 }
