625f09bf183d7e4c5cbc438c18e9b5fad884e937
김형도88 <gudeh880@naver.com>
2023-01-19 18:09:42 +0900

    네브바 위치 재조정

--- dream_com_front/src/common/NavigationBar.jsx
+++ dream_com_front/src/common/NavigationBar.jsx
@@ -17,87 +17,83 @@ function NavigationBar() {
     const [gLine, setGLine] = useState(1)
 
 
-
     return (
-        <Navbar expand="lg">
-            <div className={"container"} style={{marginLeft:"175px"}} >
+        <Navbar>
+            <div>
 
-                <Navbar.Toggle aria-controls="navbarScroll" />
-                <Navbar.Collapse id="navbarScroll">
-                    <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll>
-                        <a><li style={{listStyle: "none"}}
-                               onMouseOver={() => setBestLine(0)}
-                               onMouseOut={() => setBestLine(1)}
-                        >
-                            <Nav.Link href="#action1" style={bestLine ? {textDecorationLine: "none"} : {textDecorationLine: "underline"}}>베스트</Nav.Link>
-
-                        </li>
+                <Navbar className={"container-fluid" } style={{marginLeft:"-8px"}}>
+                    <Nav style={{maxHeight: '100px', marginLeft:"10%" }}>
+                        <a>
+                            <li style={{listStyle: "none",width:"131%",marginRight:"20px" }}
+                                onMouseOver={() => setBestLine(0)}
+                                onMouseOut={() => setBestLine(1)}
+                            >
+                                <Nav.Link href="#action1"
+                                          style={bestLine ? {textDecorationLine: "none"} : {textDecorationLine: "underline"}} >베스트</Nav.Link>
+                            </li>
                         </a>
-                        <a><li style={{listStyle: "none"}}
-                               onMouseOver={() => setSpecialLine(0)}
-                               onMouseOut={() => setSpecialLine(1)}
-                        >
-                            <Nav.Link href="#action1" style={specialLine ? {textDecorationLine: "none"} : {textDecorationLine: "underline"}}>특가상품</Nav.Link>
-
-                        </li>
+                        <a>
+                            <li style={{listStyle: "none",width:"131%",marginRight:"30px"}}
+                                onMouseOver={() => setSpecialLine(0)}
+                                onMouseOut={() => setSpecialLine(1)}
+                            >
+                                <Nav.Link href="#action1"
+                                          style={specialLine ? {textDecorationLine: "none"} : {textDecorationLine: "underline"}}>특가상품</Nav.Link>
+                            </li>
                         </a>
-                        <a><li style={{listStyle: "none"}}
-                               onMouseOver={() => setOneLine(0)}
-                               onMouseOut={() => setOneLine(1)}
-                        >
-                            <Nav.Link href="#action1" style={oneLine ? {textDecorationLine: "none"} : {textDecorationLine: "underline"}}>11번가</Nav.Link>
-
-                        </li>
+                        <a>
+                            <li style={{listStyle: "none",width:"131%",marginRight:"30px"}}
+                                onMouseOver={() => setOneLine(0)}
+                                onMouseOut={() => setOneLine(1)}
+                            >
+                                <Nav.Link href="#action1"
+                                          style={oneLine ? {textDecorationLine: "none"} : {textDecorationLine: "underline"}}>11번가</Nav.Link>
+                            </li>
                         </a>
-                        <a><li style={{listStyle: "none"}}
-                               onMouseOver={() => setZoneLine(0)}
-                               onMouseOut={() => setZoneLine(1)}
-                        >
-                            <Nav.Link href="#action1" style={zoneLine ? {textDecorationLine: "none"} : {textDecorationLine: "underline"}}>컴퓨존</Nav.Link>
-
-                        </li>
+                        <a>
+                            <li style={{listStyle: "none",width:"131%",marginRight:"30px"}}
+                                onMouseOver={() => setZoneLine(0)}
+                                onMouseOut={() => setZoneLine(1)}
+                            >
+                                <Nav.Link href="#action1"
+                                          style={zoneLine ? {textDecorationLine: "none"} : {textDecorationLine: "underline"}}>컴퓨존</Nav.Link>
+                            </li>
                         </a>
-                        <a><li style={{listStyle: "none"}}
-                               onMouseOver={() => setGLine(0)}
-                               onMouseOut={() => setGLine(1)}
-                        >
-                            <Nav.Link href="#action1" style={gLine ? {textDecorationLine: "none"} : {textDecorationLine: "underline"}}>지마켓</Nav.Link>
-
-                        </li>
+                        <a>
+                            <li style={{listStyle: "none",width:"131%",marginRight:"10px"}}
+                                onMouseOver={() => setGLine(0)}
+                                onMouseOut={() => setGLine(1)}
+                            >
+                                <Nav.Link href="#action1"
+                                          style={gLine ? {textDecorationLine: "none"} : {textDecorationLine: "underline"}}>지마켓</Nav.Link>
+                            </li>
                         </a>
-
-
-
                     </Nav>
-                    <Form className="d-flex">
 
-                        <Nav className="me-auto my-2 my-auto" style={{ maxHeight: '100px'}} navbarScroll>
 
-                            <a><li style={{listStyle: "none"}}
-                                   onMouseOver={() => setTextLine(0)}
-                                   onMouseOut={() => setTextLine(1)}
-                            >
-                                <Nav.Link href="#action1" style={textLine ? {textDecorationLine: "none"} : {textDecorationLine: "underline"}}>로그인</Nav.Link>
-
-                            </li>
+                    <Form className={"container-fluid"}>
+                        <Nav style={{marginLeft:"69%"}}>
+                            <a>
+                                <li
+                                    onMouseOver={() => setTextLine(0)}
+                                    onMouseOut={() => setTextLine(1)}
+                                >
+                                    <Nav.Link href="#action1"
+                                              style={textLine ? {textDecorationLine: "none"} : {textDecorationLine: "underline"}}>로그인</Nav.Link>
+                                </li>
                             </a>
-                            <a><li style={{listStyle: "none" ,marginRight:"-250px"}}
-                                   onMouseOver={() => setUserLine(0)}
-                                   onMouseOut={() => setUserLine(1)}
-                            >
-                                <Nav.Link href="#action2" style={userLine ? {textDecorationLine: "none"} : {textDecorationLine: "underline"}}>회원가입</Nav.Link>
-
-                            </li>
+                            <a>
+                                <li
+                                    onMouseOver={() => setUserLine(0)}
+                                    onMouseOut={() => setUserLine(1)}
+                                >
+                                    <Nav.Link href="#action2"
+                                              style={userLine ? {textDecorationLine: "none"} : {textDecorationLine: "underline"}}>회원가입</Nav.Link>
+                                </li>
                             </a>
-
-
-
-
                         </Nav>
                     </Form>
-
-
-                </Navbar.Collapse>
+                </Navbar>
             </div>
         </Navbar>
     );
