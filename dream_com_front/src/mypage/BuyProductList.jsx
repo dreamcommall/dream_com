8c692fbadf94866d73407a017ec9d8e42337c7ea
YOUNGMAN\dudal <dudals334@naver.com>
2023-01-31 12:43:09 +0900

    xxx

--- dream_com_front/src/mypage/BuyProductList.jsx
+++ dream_com_front/src/mypage/BuyProductList.jsx
@@ -27,7 +27,7 @@ function BuyProductList() {
                         </tr>
                         </thead>
                         <tbody>
-                        <tr>
+                        <tr className={"tableHover"}>
                             {/* 상품정보 */}
                             <td>
                                 <div className={"mt-0"}>
@@ -38,7 +38,20 @@ function BuyProductList() {
                                                 M100
                                             </div>
                                             <div className={"nanumSquareR-font-small mt-2"}>1,500,000 원</div>
-                                            <div className={"nanumSquareR-font-small mt-2"}>옵션</div>
+                                            <div className="accordion nanumSquareR-font-normal mt-3" id="accordionExample">
+                                                <div className="accordion-item">
+                                                    <p className="accordion-header" id="headingOne">
+                                                        <button type="button" data-bs-toggle="collapse" className={"accordionButton"}
+                                                                data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                                            내가 쓴 리뷰 보기
+                                                        </button>
+                                                    </p>
+                                                    <div id="collapseOne" className="accordion-collapse collapse"
+                                                         aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                                                        <div className={"ms-2 pt-1"}>포토샵을 할 수 있는 노트북 찾다가 이걸로 골랐어요 그전에 쓰던 다른 노트북보다 화면은 큰데 훨씬 얇고 가볍네요</div>
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </li>
                                     </ul>
                                 </div>
@@ -123,4 +136,4 @@ function BuyProductList() {
     )
 }
 
-export default BuyProductList
+export default BuyProductList;
--- dream_com_front/src/mypage/MypageCart.jsx
+++ dream_com_front/src/mypage/MypageCart.jsx
@@ -1,9 +1,79 @@
-import React from "react";
+import React, {useState} from "react";
+import "./MypageCart.css"
 
 function MypageCart(){
+    const [quantity,setQuantity] = useState(0);
+    const handleChange = (e) => {
+        setQuantity(e.target.value);
+    }
+    const plusQuantity = () =>{
+        setQuantity(quantity +1);
+    }
+    const minusQuantity = () =>{
+        setQuantity(quantity -1);
+    }
     return(
         <div className={"container"}>
-            <div>ddd</div>
+            <h3 className={"mt-5"}>Order / Payment</h3>
+            <table className={"table mt-5 nanumSquareR-font-small"}>
+                <thead className={"text-center title"}>
+                    <tr>
+                        <th className={"col-5"}>상품명</th>
+                        <th className={"col-2"}>판매가</th>
+                        <th className={"col-1"}>할인</th>
+                        <th className={"col-1"}>수량</th>
+                        <th className={"col-2"}>주문금액</th>
+                        <th className={"col-1"}>배송 형태</th>
+                    </tr>
+                </thead>
+
+                <tbody className={"text-center"}>
+                    <tr>
+                        <td>
+                            <div className={"img-fluid ms-1"}>
+                                <a href={"#"}><img src={"images/logo192.png"} /></a>
+                                <div style={{marginLeft:"100px"}}>
+                                    <a href={"#"} className={"productTitle"}>
+                                        <div className={"mt-0"}>[레노버] ThinkStation P358 TWR-30GLS00A00 AMD R7-5845 [16G/512G/T1000/Win11Pro] [기본제품]</div>
+                                    </a>
+                                    <div className={"mt-3 option"}>
+                                        <div>옵션</div>
+                                        <div>AMD Ryzen 7 PRO 5845, NVIDIA T1000 8GB GDDR6 HP, 16GB DDR 3200 NDIMM 1RX8</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </td>
+                        <td>
+                            <div className={"py-5"}>3,000,000</div>
+                        </td>
+                        <td>
+                            <div className={"py-5"}>-20,000</div>
+                        </td>
+                        <td id={"Quantity"}>
+                            <button onClick={minusQuantity}>-</button>
+                            <input value={quantity} onChange={handleChange} />
+                            <button onClick={plusQuantity}>+</button>
+                        </td>
+                        <td>
+                            <div className={"py-5"}>2,980,000</div>
+                        </td>
+                        <td>
+                            <div className={"py-5"}>당일배송</div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <div className={"text-center nanumSquareR-font-normal"}>
+                <div className={"totalPrice"}>총 상품금액: 3,000,000</div>
+                <div className={"float-start ms-4"}>-</div>
+                <div className={"totalSale"}>총 할인금액 : 20,000</div>
+                <div className={"float-start ms-4"}>=</div>
+                <div className={"totalMoney ms-4"}>총 금액 : 2,980,000</div>
+            </div>
+            <br/>
+            <br/>
+            <button className={"deleteCart"}>장바구니 비우기</button>
+            <button className={"buyCart"}>구매하기</button>
         </div>
     )
 }
