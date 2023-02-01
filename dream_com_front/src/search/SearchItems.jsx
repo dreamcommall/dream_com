5f92c8d137f652cba5416fa3bba7da59b7f26c95
MoonNight285 <65889750+MoonNight285@users.noreply.github.com>
2023-02-01 21:27:21 +0900

    검색페이지 검색된 항목에서 별점이 제대로 표시가 안되는 문제 수정

--- dream_com_front/src/search/SearchItems.jsx
+++ dream_com_front/src/search/SearchItems.jsx
@@ -3,15 +3,15 @@ import SearchItem from "./SearchItem";
 import "../fonts/fontStyle.css"
 
 function SearchItems({searchItemInfo}) {
+    console.log(searchItemInfo);
+
     return (
         <div className={"my-4"} style={searchItemInfo.length == 0 ? {width : "100%"} : {width : "100%", borderBottom : "1px solid lightgray"}}>
             {
                 searchItemInfo.length == 0 ? <div style={{height : 375}} className={"d-flex justify-content-center align-items-center"}>
                         <p className={"nanumSquareB-font-XNormal"}>조회된 결과가 없습니다.</p></div> :
                 searchItemInfo.map(item => {
-                    return <SearchItem key={item.key} src={item.thumbnailImg} title={item.productTitle} specList={item.partName} averageScore={item.score}
-                        registrationDate={item.productCreateDt} commentCount={item.reviewNumber} price={item.productPrice}
-                    discount={item.productDiscount}/>
+                    return <SearchItem key={item.key} searchItemInfo={item}/>
                 })
             }
         </div>
