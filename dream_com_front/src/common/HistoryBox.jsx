166c5f6590edae4933dd02e5d01bff942300a5d9
MoonNight285 <kimjyjh11@naver.com>
2023-02-02 14:09:21 +0900

    세션 스토리지를 이용해 히스토리에 상품을 채우는 기능 추가

--- dream_com_front/src/common/HistoryBox.jsx
+++ dream_com_front/src/common/HistoryBox.jsx
@@ -1,22 +1,23 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import "../fonts/fontStyle.css";
 import "./HistoryBox.css";
 import {getAllHistory} from "./js/sessionStorageManager";
 
 // 작성자 : MoonNight285
 // 히스토리 박스를 그려주는 컴포넌트
-// itemList => 최신 데이터를 기준으로 방문했던 상품의 이미지를 리스트로 받아옴(최대 4개 표시)
-function HistoryBox({itemList}) {
+function HistoryBox() {
+    const [historyList, setHistoryList] = useState([]); // 히스토리 상품이 담긴 배열
+    
+    // 세션 스토리지의 길이가 달라졌거나, 첫번째 상품의 이름의 값이 달라진경우 갱신
     useEffect(() => {
-        // sessionStorage.clear();
-        console.log(getAllHistory());
-    }, []);
+        setHistoryList(getAllHistory());
+    }, [sessionStorage.length, sessionStorage.getItem(`productNum0`)]);
     
     return (
         <div id={"div-history-box"}>
             <h6 className={"text-center p-2 nanumSquareB-font-normal"}>히스토리</h6>
             {
-                itemList.length == 0 ?
+                historyList.length == 0 ?
                     <div className={"d-flex justify-content-center align-items-center"} style={{height : "80%"}}>
                         <div className={"text-center"}>
                             <p className={"mb-0 nanumSquareR-font-normal"}>최근에 본</p>
@@ -27,10 +28,10 @@ function HistoryBox({itemList}) {
                     :
                     <div className={"text-center"}>
                         {
-                            itemList.map(item => {
+                            historyList.map(item => {
                                 return (
-                                    <div key={item.key}>
-                                        <a href={"#"}><img className={"mb-3"} width={70} height={70} src={item.src} /></a>
+                                    <div key={item.key} title={item.productTitle}>
+                                        <a href={`/detail?productNum=${item.productNum}`}><img className={"mb-3"} width={70} height={70} src={item.thumbnailImg} /></a>
                                     </div>
                                 );
                             })
