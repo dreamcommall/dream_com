44fa68f3258315471a609a6ddbf4dd624a1f5521
김형도88 <gudeh880@naver.com>
2023-02-06 17:28:14 +0900

    회원가입 헤더 사이드 메뉴 페이지별 색상 추가

--- dream_com_front/src/SignUp/SignUpHeader.jsx
+++ dream_com_front/src/SignUp/SignUpHeader.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import "./SignUpHeader.css";
 import SignMainLogo from "../SignMainLogo.png"
-import SignInfomation from "../common/SignInfomation";
 import "../fonts/fontStyle.css";
+import {useLocation} from "react-router-dom";
 
 function SignUpHeader() {
+
+
+
+    // const location=useLocation();
+    // console.log(location);
+
+    const {pathname} = useLocation()
+    console.log(pathname);
+
+
     return (
         <div>
             <div className={"container"} id={"signMain"}>
@@ -17,14 +27,20 @@ function SignUpHeader() {
                     <p style={{fontSize:"40px", paddingTop:"10px"}} className={"nanumSquareB-font-normal"}>회원가입</p>
                 </div>
                 <div className={"ListHeader"} >
-                    <div id={"yac"}>
-                        <p className={"nanumSquareB-font-normal"}>약관 동의</p>
+                    <div id={"yac"} >
+                        <p style={{
+                            color: pathname == "/sign" ? "red" : "black"
+                        }} className={"nanumSquareB-font-normal"}>약관 동의</p>
                     </div>
                     <div id={"information"}>
-                        <p className={"nanumSquareB-font-normal"}>정보 입력</p>
+                        <p style={{
+                            color: pathname == "/signInfomation" ? "red" : "black"
+                        }} className={"nanumSquareB-font-normal"}>정보 입력</p>
                     </div>
                     <div id={"SignUpClear"}>
-                        <p className={"nanumSquareB-font-normal"}>가입 완료</p>
+                        <p style={{
+                            color: pathname == "/clearTitle/signClear" ? "red" : "black"
+                        }} className={"nanumSquareB-font-normal"}>가입 완료</p>
                     </div>
                 </div>
             </div>
