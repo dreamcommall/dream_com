ee8868bd92735744580648732f288b0f65027d9f
MoonNight285 <kimjyjh11@naver.com>
2023-02-08 15:25:36 +0900

    마이페이지 메뉴바 위치 수정 및 링크 수정

--- dream_com_front/src/mypage/MyPageNav.jsx
+++ dream_com_front/src/mypage/MyPageNav.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import "./MyPageNav.css"
 import "../fonts/fontStyle.css"
+import {Link} from "react-router-dom";
 
 function MyPageNav({menu}) {
     return (
         <div id={"div-my-page-nav"}>
             <h4 className={"my-2 p-1 nanumSquareB-font-large"}>마이페이지</h4>
-            <a href={"http://localhost:3000/mypageOrder"}><p className={menu == "order" ? "active" : ""}>주문배송 & 구매내역</p></a>
-            <a href={"http://localhost:3000/mypageCart"}><p className={menu == "wishList" ? "active" : ""}>찜목록</p></a>
-            <a href={"http://localhost:3000/mypageWishList"}><p className={menu == "cart" ? "active" : ""}>장바구니</p></a>
+            <Link to={"/mypageOrder"}><p className={menu == "order" ? "active" : ""}>주문배송 & 구매내역</p></Link>
+            <Link to={"/mypageCart"}><p className={menu == "wishList" ? "active" : ""}>찜목록</p></Link>
+            <Link to={"/mypageWishList"}><p className={menu == "cart" ? "active" : ""}>장바구니</p></Link>
             <p className={menu == "userEdit" ? "active" : ""}>개인정보수정</p>
         </div>
     );
