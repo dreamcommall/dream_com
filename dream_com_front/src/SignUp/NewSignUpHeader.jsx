1b230f6f59248cf0f8ba03cbacfeb5f04ab72e51
MoonNight285 <kimjyjh11@naver.com>
2023-02-13 21:10:21 +0900

    회원가입 헤더 부분 컴포넌트 작성

---
+++ dream_com_front/src/SignUp/NewSignUpHeader.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "../fonts/fontStyle.css"
+import "./NewSignUpHeader.css"
+
+function NewSignUpHeader({pageName}) {
+    return (
+        <div className={"container-fluid"}>
+            <div id={"div-sing-up-header"} className={"d-flex"}>
+                <div>
+                    <img width={120} height={60} src={"/images/mainLogo.png"} />
+                </div>
+                <div>
+                    <p id={"p-sing-up-header-information"}>회원가입</p>
+                </div>
+                <div className={"d-flex align-items-center"}>
+                    <p className={pageName == "Clause" ? "p-sing-up-header-page-information active" : "p-sing-up-header-page-information"}>약관동의</p>
+                    <p className={pageName == "SignUp" ? "p-sing-up-header-page-information active" : "p-sing-up-header-page-information"}>정보입력</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default NewSignUpHeader;
