164bb1ed40f98302db0aabdac7ec3d9417cd4d87
yang <ymh0052@naver.com>
2023-02-13 16:47:00 +0900

    경로 변경

--- dream_com_front/src/mypage/MyPageNav.jsx
+++ dream_com_front/src/mypage/MyPageNav.jsx
@@ -8,8 +8,8 @@ function MyPageNav({menu}) {
         <div id={"div-my-page-nav"}>
             <h4 className={"my-2 p-1 nanumSquareB-font-large"}>마이페이지</h4>
             <Link to={"/mypage/order"}><p className={menu == "order" ? "active" : ""}>주문배송 & 구매내역</p></Link>
-            <Link to={"/mypageWishList"}><p className={menu == "wishList" ? "active" : ""}>찜목록</p></Link>
-            <Link to={"/mypageCart"}><p className={menu == "cart" ? "active" : ""}>장바구니</p></Link>
+            <Link to={"/mypage/wishList"}><p className={menu == "wishList" ? "active" : ""}>찜목록</p></Link>
+            <Link to={"/mypage/cart"}><p className={menu == "cart" ? "active" : ""}>장바구니</p></Link>
             <p className={menu == "userEdit" ? "active" : ""}>개인정보수정</p>
         </div>
     );
